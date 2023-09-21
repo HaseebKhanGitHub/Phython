1306c6e0607c513e56f38d72277e68ae665f0d65
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-09-21 23:21:32 +0500

    Create Odd_func.py

---
+++ Odd_func.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 00:03:35 2023
+
+@author: khanb
+"""
+
+def odd(x):
+    '''
+    x: int or float.
+    '''
+    return x % 2 != 0
+
+result1 = odd(3) 
+result2 = odd(4)  
+
+print(result1)
+print(result2)
