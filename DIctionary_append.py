d5320315b45e41327639cf2bd7ae02e362b27d0b
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-09-25 23:22:59 +0500

    Create DIctionary_append.py

---
+++ DIctionary_append.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 22:55:23 2023
+
+@author: khanb
+"""
+
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+print(animals)
