384b0f69d11cfad66cb2969d1ea1c84f1338288a
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-09-30 23:58:56 +0500

    Update Factorial_check.py

--- Factorial_check.py
+++ Factorial_check.py
@@ -11,6 +11,6 @@ if num<0:
 elif (num==0):
     print("factorial of 0 is 1")
 else:
-    for i in range(1, num-1):
+    for i in range(1, num+1):
         factorial=factorial*i
     print("the factorial of ",num," is ", factorial)
