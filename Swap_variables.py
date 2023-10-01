714fbdfe4a234850607047040b1fc4baceb789c0
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-01 22:24:16 +0500

    Create Swap_variables.py

---
+++ Swap_variables.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 22:18:56 2023
+
+@author: khanb
+"""
+
+x=5
+y=10
+
+temp=x
+x=y
+y=temp
+
+print('value of x is: {}'.format(x))
+print('value of y is: {}'.format(y))
+
