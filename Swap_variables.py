ad6169b89d99d48565be0e88c318ee1e6367c063
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-01 22:28:05 +0500

    Update Swap_variables.py

--- Swap_variables.py
+++ Swap_variables.py
@@ -4,7 +4,7 @@ Created on Sun Oct  1 22:18:56 2023
 
 @author: khanb
 """
-
+'''
 x=5
 y=10
 
@@ -15,3 +15,10 @@ y=temp
 print('value of x is: {}'.format(x))
 print('value of y is: {}'.format(y))
 
+'''
+x = 5
+y = 10
+
+x, y = y, x
+print("x = ",x)
+print("y = ",y)
