cca1e9578ebc521568cd7f808b542bdbf81b3d13
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-05 23:52:01 +0500

    Create Fount_it_question.py

---
+++ Fount_it_question.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 09:45:45 2023
+
+@author: khanb
+"""
+
+stuff  = [ ( "iBoy", "iGirl", "iQ", "iC","iPaid","iPad") ]
+for thing in stuff:
+    if thing == 'iQ':
+        print("Found it")
