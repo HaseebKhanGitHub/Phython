90a851413dfd7dd5bfa694448c3386080c148e22
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-15 21:31:32 +0500

    Create Remainder_function.py

---
+++ Remainder_function.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 21:29:18 2023
+
+@author: khanb
+"""
+
+def rem(x, a):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the remainder when x is divided by a.
+    """
+    if x == a:
+        return 0
+    elif x < a:
+        return x
+    else:
+        rem(x-a, a)
+res=rem(2,5)
+print(res)
