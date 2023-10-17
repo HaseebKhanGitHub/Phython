5be93e9f02bfbd4e56d91d9e34e526742df2167b
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-17 22:42:15 +0500

    Update Histogram_equalization.py

--- Histogram_equalization.py
+++ Histogram_equalization.py
@@ -9,7 +9,6 @@ import numpy as np
 import cv2
     
 def histogram_equalization(image):
-    # Calculate the histogram of the input image
     height, width = image.shape
     histogram = np.zeros(256, dtype=int)
     
@@ -18,14 +17,13 @@ def histogram_equalization(image):
             pixel_value = image[i, j]
             histogram[pixel_value] += 1
             
-    # Calculate the cumulative distribution function (CDF)
     cdf = np.zeros(256, dtype=float)
     cdf[0] = histogram[0]
     
     for i in range(1, 256):
         cdf[i] = cdf[i - 1] + histogram[i]
+
     
-    # Perform histogram equalization
     equalized_image = np.zeros_like(image)
     num_pixels = height * width
     
@@ -37,17 +35,13 @@ def histogram_equalization(image):
     
     return equalized_image
 
-# Load an image
-input_image = cv2.imread('Input.PNG', cv2.IMREAD_GRAYSCALE)
+input_image = cv2.imread('g.png', cv2.IMREAD_GRAYSCALE)
 
-# Perform histogram equalization
 equalized_image = histogram_equalization(input_image)
 
-# Display the original and equalized images
 cv2.imshow('Original Image', input_image)
 cv2.imshow('Equalized Image', equalized_image)
 
-# Wait for a key press and then close the windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
