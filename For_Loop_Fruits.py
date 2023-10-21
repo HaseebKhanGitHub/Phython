f75e8f113f396f7c3632758ebd4ecabebab191cb
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-21 23:58:03 +0500

    Create For_Loop_Fruits.py

---
+++ For_Loop_Fruits.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 23:57:40 2023
+
+@author: khanb
+"""
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
