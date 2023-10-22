3c48e2c11544c6f45a0a1c914fb1a7f0ee48344b
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-23 00:53:47 +0500

    Create Cloning_list.py

---
+++ Cloning_list.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 22 21:40:10 2023
+
+@author: khanb
+"""
+
+cool=['green', 'blue','grey']
+
+chill=cool[:]
+
+chill.append('white')
+
+print(cool)
+print(chill)
