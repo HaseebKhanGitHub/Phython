1a440f77e52c3d15044721648f10e1d2b2cd52f9
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-10-23 00:53:52 +0500

    Create Sorting_list.py

---
+++ Sorting_list.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 22 21:44:05 2023
+
+@author: khanb
+"""
+
+#changes original
+warm=['red','black']
+sortedWarm = warm.sort()
+print(warm)
+print(sortedWarm)
+
+#does not changes actual
+cool=['blue','voilet']
+sortedCool= sorted(cool)
+print(cool)
+print(sortedCool)
