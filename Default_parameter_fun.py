adc1ff2911b435680a4bf115190f8d1de496ce4f
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-01-03 22:43:06 +0500

    Create Default_parameter_fun.py

---
+++ Default_parameter_fun.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 22:35:28 2024
+
+@author: khanb
+"""
+
+def my_function(country="Pakistan"):
+    print("I am from "+ country)
+    
+my_function()
+my_function("Bangladesh")
+my_function("Turkey")
