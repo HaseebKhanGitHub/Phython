58ac56fcd5cb27beb67a9b3c8b565e429ee408a8
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-01-07 21:15:09 +0500

    Create Iterator.py

---
+++ Iterator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 21:12:37 2024
+
+@author: khanb
+"""
+
+myTuple=("apple", "banana", "cherry")
+myitr= iter(myTuple)
+
+print(next(myitr))
+print(next(myitr))
+print(next(myitr))
