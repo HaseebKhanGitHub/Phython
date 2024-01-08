8ef2b01d88035b392c81ff26a3238ad8144d2580
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-01-08 23:40:44 +0500

    Create myModule2.py

---
+++ myModule2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 23:36:36 2024
+
+@author: khanb
+"""
+
+import myModule
+
+
+myModule.greetings("Haseeb Khan")
+
+#call module1 function by just importing function
