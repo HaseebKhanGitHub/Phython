318f04e66e34d7afb7ea38a8791f595a398bc683
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-01-08 23:40:50 +0500

    Create myModule.py

---
+++ myModule.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 23:34:24 2024
+
+@author: khanb
+"""
+
+def greetings(name):
+    print("hello  "+ name)
+    
+#save a file like a library with function
