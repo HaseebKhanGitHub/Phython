afee64304008d494f403b728331d7e741df8da40
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-01-13 20:04:51 +0500

    Create Dictionary_value_onebyone.py

---
+++ Dictionary_value_onebyone.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 20:03:58 2024
+
+@author: khanb
+"""
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])
