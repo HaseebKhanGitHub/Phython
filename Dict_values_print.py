4f39bec08b0b5009711b994db1b1d2c6bcb40ef5
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-01-15 23:36:10 +0500

    Create Dict_values_print.py

---
+++ Dict_values_print.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 23:34:20 2024
+
+@author: khanb
+"""
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])
