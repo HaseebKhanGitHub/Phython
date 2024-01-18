40fd317166b22bb9a5c276a176c6fe2ca52ba62b
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-01-18 18:26:13 +0500

    Create Key_exist.py

---
+++ Key_exist.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 18:25:39 2024
+
+@author: khanb
+"""
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
